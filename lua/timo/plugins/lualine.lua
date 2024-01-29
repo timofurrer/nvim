3848b85e6ebac308ddf3e32fcf71b80744272bd5
Timo Furrer <tuxtimo@gmail.com>
2024-01-29 19:17:33 +0100

    Add lsp info to lualine

--- lua/timo/plugins/lualine.lua
+++ lua/timo/plugins/lualine.lua
@@ -3,6 +3,32 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {},
+    opts = {
+      sections = {
+        lualine_c = {
+          'filename',
+          {
+            -- Lsp server name, from https://github.com/jpmcb/nvim-lua-conf/blob/ec6fba3ea21ed851bed5c493847e0f88152a4f03/plugin/lualine.lua#L160
+            function()
+              local msg = "No Active Lsp"
+              local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
+              local clients = vim.lsp.get_active_clients()
+              if next(clients) == nil then
+                return msg
+              end
+              for _, client in ipairs(clients) do
+                local filetypes = client.config.filetypes
+                if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+                  return client.name
+                end
+              end
+              return msg
+            end,
+            icon = " LSP:",
+            color = { fg = "#ffffff", gui = "bold" },
+          }
+        },
+      }
+    },
   }
 }
