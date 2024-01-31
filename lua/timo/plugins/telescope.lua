2ef5a5ee032198f3becf9fd6f8869bc54701ba34
Timo Furrer <tuxtimo@gmail.com>
2024-01-31 12:03:27 +0100

    Lazy start telescope so that the dashboard has access to it

--- lua/timo/plugins/telescope.lua
+++ lua/timo/plugins/telescope.lua
@@ -9,6 +9,7 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     dependencies = { 'nvim-lua/plenary.nvim' },
+    event = { "VeryLazy" },
     keys = {
       { "<leader>ff", function() require("telescope.builtin").find_files({ hidden = true, file_ignore_patterns = file_ignore_patterns }) end, desc = "Find Files" },
       { "<leader>fg", function() require("telescope.builtin").git_files() end, desc = "Find Git Files" },
